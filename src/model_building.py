52b81a90cb83da2b0747469091d08e63eec49d1c
curiouscurrent <divakardivi1963@gmail.com>
2026-04-13 00:20:58 +0530

    model training step updated

--- src/model_building.py
+++ src/model_building.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 import logging
 from sklearn.ensemble import RandomForestClassifier
-import yaml
+
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -28,23 +28,6 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-def load_params(params_path: str) -> dict:
-    """Load parameters from a YAML file."""
-    try:
-        with open(params_path, 'r') as file:
-            params = yaml.safe_load(file)
-        logger.debug('Parameters retrieved from %s', params_path)
-        return params
-    except FileNotFoundError:
-        logger.error('File not found: %s', params_path)
-        raise
-    except yaml.YAMLError as e:
-        logger.error('YAML error: %s', e)
-        raise
-    except Exception as e:
-        logger.error('Unexpected error: %s', e)
-        raise
-
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
@@ -67,6 +50,7 @@ def load_data(file_path: str) -> pd.DataFrame:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
+# Load the data from processed folder (i.e Feature engineering component)
 def train_model(X_train: np.ndarray, y_train: np.ndarray, params: dict) -> RandomForestClassifier:
     """
     Train the RandomForest model.
